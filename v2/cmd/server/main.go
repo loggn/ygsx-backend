9d287a175e540fe724433e356aacefb670be4be2
loggn <1137127358@qq.com>
2025-12-30 00:10:19 +0800

    添加Gin框架

--- v2/cmd/server/main.go
+++ v2/cmd/server/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"ygsx-v2/internal/router"
+)
 
 func main() {
+	r := router.NewRouter()
+
 	fmt.Println("鸭古生鲜SaaS服务器运行中...")
+
+	if err := r.Run(":8000"); err != nil {
+		log.Fatal(err)
+	}
+
 }
--- v2/internal/router/router.go
+++ v2/internal/router/router.go
@@ -1,1 +1,15 @@
 package router
+
+import "github.com/gin-gonic/gin"
+
+func NewRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "连接正常",
+		})
+	})
+
+	return r
+}
